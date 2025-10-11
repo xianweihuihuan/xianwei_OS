6c06627ff4c8ff1e80ba9abdc48c365e6f48f72b
xianweihuihuan <2907404984@qq.com>
2025-10-11 16:53:45 +0800

    init

--- kernel/init.c
+++ kernel/init.c
@@ -3,11 +3,19 @@
 #include "interrupt.h"
 #include "print.h"
 #include "memory.h"
-
+#include "thread.h"
+#include "console.h"
+#include "keyboard.h"
+#include "tss.h"
+//进行必要的初始化
 void init_all() {
   put_str("init all\n");
-  idt_init();
-  timer_init();
-  mem_init();
+  idt_init();//有关中断额的初始化
+  mem_init();//初始化内存池
+  thread_init();
+  timer_init();//初始化时钟中断的频率
+  console_init();
+  keyboard_init();
+  tss_init();
   put_str("init all done\n");
 }
