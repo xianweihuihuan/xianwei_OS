a540a30bb8332c5d2e098c5914e289282ce44304
xianweihuihuan <2907404984@qq.com>
2025-10-11 17:01:29 +0800

    main

--- kernel/main.c
+++ kernel/main.c
@@ -1,36 +1,63 @@
+#include "console.h"
 #include "debug.h"
 #include "init.h"
-#include "memory.h"
-#include "print.h"
-#include "thread.h"
-#include "timer.h"
 #include "interrupt.h"
-#include "console.h"
 #include "ioqueue.h"
 #include "keyboard.h"
+#include "memory.h"
+#include "print.h"
+#include "process.h"
+#include "thread.h"
+#include "timer.h"
+void k_thread_a(void*);
+void k_thread_b(void*);
+void u_prog_a(void);
+void u_prog_b(void);
+int test_var_a = 0, test_var_b = 0;
 
-void k_thread(void* arg);
-
-int main() {
-  put_str("This is XM's kernel\n");
+int main(void) {
+  put_str("I am kernel\n");
   init_all();
-  thread_start("k_thread_a", 31, k_thread, " A_");
-  thread_start("k_thread_b", 8, k_thread, " B_");
+
+  thread_start("k_thread_a", 31, k_thread_a, "argA ");
+  thread_start("k_thread_b", 31, k_thread_b, "argB ");
+  process_execute(u_prog_a, "user_prog_a");
+  process_execute(u_prog_b, "user_prog_b");
+
   intr_enable();
+  while (1)
+    ;
+  return 0;
+}
+
+/* 在线程中运行的函数 */
+void k_thread_a(void* arg) {
+  char* para = arg;
   while (1) {
-    // console_put_str("Main ");
+    console_put_str(" v_a:0x");
+    console_put_int(test_var_a);
   }
 }
 
-void k_thread(void* arg) {
+/* 在线程中运行的函数 */
+void k_thread_b(void* arg) {
   char* para = arg;
   while (1) {
-    enum intr_status old_status = intr_disable();
-    if(!ioq_empty(&kbd_buf)){
-      console_put_str(arg);
-      char byte = ioq_getchar(&kbd_buf);
-      console_put_char(byte);
-    }
-    intr_set_status(old_status);
+    console_put_str(" v_b:0x");
+    console_put_int(test_var_b);
+  }
+}
+
+/* 测试用户进程 */
+void u_prog_a(void) {
+  while (1) {
+    test_var_a++;
+  }
+}
+
+/* 测试用户进程 */
+void u_prog_b(void) {
+  while (1) {
+    test_var_b++;
   }
 }
