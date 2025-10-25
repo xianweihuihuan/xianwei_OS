c72f50b13e69034590754f5cdcb2c6c06b6d0e61
xianweihuihuan <2907404984@qq.com>
2025-10-25 17:02:18 +0800

    file

---
+++ fs/file.h
@@ -0,0 +1,22 @@
+#ifndef __FS_FILE_H
+#define __FS_FILE_H
+#include "stdint.h"
+#include "inode.h"
+
+struct file{
+  uint32_t fd_pos;
+  uint32_t fd_flag;
+  struct inode* fd_inode;
+};
+
+enum std_fd { stdin_no, stdout_no, stderr_no };
+
+enum bitmap_type { INODE_BITMAP, BLOCK_BITMAP };
+int32_t get_free_slot_in_global();
+int32_t pcb_fd_install(int32_t globa_fd_idx);
+int32_t inode_bitmap_alloc(struct partition* part);
+int32_t block_bitmap_alloc(struct partition* part);
+void bitmap_sync(struct partition* part, uint32_t bit_idx, uint8_t btmp);
+
+#define MAX_FILES_OPEN 32
+#endif
