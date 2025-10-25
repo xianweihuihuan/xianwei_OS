ca203588e35540988a28acc45908f0e8462c29c6
xianweihuihuan <2907404984@qq.com>
2025-10-25 17:09:24 +0800

    syscall-init

---
+++ userprog/syscall-init.c
@@ -0,0 +1,30 @@
+#include "syscall-init.h"
+#include "syscall.h"
+#include "thread.h"
+#include "print.h"
+#include "string.h"
+#include "console.h"
+
+#define syscall_nr 32
+typedef void* syscall;
+syscall syscall_table[syscall_nr];
+
+uint32_t sys_getpid(){
+  return running_thread()->pid;
+}
+
+uint32_t sys_write(char* str){
+  console_put_str(str);
+  return strlen(str);
+}
+
+void syscall_init(){
+  put_str("  syscall_init start\n");
+  syscall_table[SYS_GETPID] = sys_getpid;
+  syscall_table[SYS_WRITE] = sys_write;
+  syscall_table[SYS_MALLOC] = sys_malloc;
+  syscall_table[SYS_FREE] = sys_free;
+  put_str("  syscall_init done\n");
+}
+
+
---
+++ userprog/syscall-init.h
@@ -0,0 +1,7 @@
+#ifndef __USERPROG_SYSCALLINIT_H
+#define __USERPROG_SYSCALLINIT_H
+#include "stdint.h"
+uint32_t sys_write(char* str);
+void syscall_init();
+
+#endif
