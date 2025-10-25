a40794889878b980e3848996577a4e32ee08535a
xianweihuihuan <2907404984@qq.com>
2025-10-25 17:06:11 +0800

    list

--- lib/kernel/list.h
+++ lib/kernel/list.h
@@ -1,5 +1,5 @@
-#ifndef __KERNEL_LIST_H
-#define __KERNEL_LIST_H
+#ifndef __LIB_KERNEL_LIST_H
+#define __LIB_KERNEL_LIST_H
 #include "global.h"
 
 #define offset(struct_type,member) (int)(&((struct_type*)0)->member)
