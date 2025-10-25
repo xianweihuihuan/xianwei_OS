8606c8097444dc28d0c68bceccfc5e7f69bb46cd
xianweihuihuan <2907404984@qq.com>
2025-10-25 17:04:45 +0800

    main

--- kernel/main.c
+++ kernel/main.c
@@ -1,63 +1,102 @@
-#include "console.h"
-#include "debug.h"
-#include "init.h"
-#include "interrupt.h"
-#include "ioqueue.h"
-#include "keyboard.h"
-#include "memory.h"
 #include "print.h"
-#include "process.h"
+#include "init.h"
 #include "thread.h"
+#include "interrupt.h"
+#include "console.h"
+#include "process.h"
+#include "syscall-init.h"
+#include "syscall.h"
+#include "stdio.h"
 #include "timer.h"
+#include "memory.h"
+#include "stdio-kernel.h"
+
 void k_thread_a(void*);
 void k_thread_b(void*);
 void u_prog_a(void);
 void u_prog_b(void);
-int test_var_a = 0, test_var_b = 0;
 
 int main(void) {
-  put_str("I am kernel\n");
-  init_all();
-
-  thread_start("k_thread_a", 31, k_thread_a, "argA ");
-  thread_start("k_thread_b", 31, k_thread_b, "argB ");
-  process_execute(u_prog_a, "user_prog_a");
-  process_execute(u_prog_b, "user_prog_b");
-
-  intr_enable();
-  while (1)
-    ;
-  return 0;
+   put_str("I am kernel\n");
+   init_all();
+   //process_execute(u_prog_a, "u_prog_a");
+   //process_execute(u_prog_b, "u_prog_b");
+   //thread_start("k_thread_a", 31, k_thread_a, "I am thread_a");
+   //thread_start("k_thread_b", 31, k_thread_b, "I am thread_b");
+   intr_enable();
+   while (1)
+     ;
+   return 0;
 }
 
 /* 在线程中运行的函数 */
-void k_thread_a(void* arg) {
-  char* para = arg;
-  while (1) {
-    console_put_str(" v_a:0x");
-    console_put_int(test_var_a);
-  }
+void k_thread_a(void* arg) {     
+   void* addr1 = sys_malloc(256);
+   void* addr2 = sys_malloc(255);
+   void* addr3 = sys_malloc(254);
+   console_put_str(" thread_a malloc addr:0x");
+   console_put_int((int)addr1);
+   console_put_char(',');
+   console_put_int((int)addr2);
+   console_put_char(',');
+   console_put_int((int)addr3);
+   console_put_char('\n');
+
+   int cpu_delay = 9999999;
+   while(cpu_delay-- > 0);
+   sys_free(addr1);
+   sys_free(addr2);
+   sys_free(addr3);
+   while(1);
 }
 
 /* 在线程中运行的函数 */
-void k_thread_b(void* arg) {
-  char* para = arg;
-  while (1) {
-    console_put_str(" v_b:0x");
-    console_put_int(test_var_b);
-  }
+void k_thread_b(void* arg) {     
+   void* addr1 = sys_malloc(256);
+   void* addr2 = sys_malloc(255);
+   void* addr3 = sys_malloc(254);
+   console_put_str(" thread_b malloc addr:0x");
+   console_put_int((int)addr1);
+   console_put_char(',');
+   console_put_int((int)addr2);
+   console_put_char(',');
+   console_put_int((int)addr3);
+   console_put_char('\n');
+
+   int cpu_delay = 999999;
+   while(cpu_delay-- > 0);
+   sys_free(addr1);
+   sys_free(addr2);
+   sys_free(addr3);
+   while(1);
 }
 
 /* 测试用户进程 */
 void u_prog_a(void) {
-  while (1) {
-    test_var_a++;
-  }
+   void* addr1 = malloc(256);
+   void* addr2 = malloc(255);
+   void* addr3 = malloc(254);
+   printf(" prog_a malloc addr:0x%x,0x%x,0x%x\n", (int)addr1, (int)addr2, (int)addr3);
+
+   int cpu_delay = 100000;
+   while(cpu_delay-- > 0);
+   free(addr1);
+   free(addr2);
+   free(addr3);
+   while(1);
 }
 
 /* 测试用户进程 */
 void u_prog_b(void) {
-  while (1) {
-    test_var_b++;
-  }
+   void* addr1 = malloc(256);
+   void* addr2 = malloc(255);
+   void* addr3 = malloc(254);
+   printf(" prog_b malloc addr:0x%x,0x%x,0x%x\n", (int)addr1, (int)addr2, (int)addr3);
+
+   int cpu_delay = 100000;
+   while(cpu_delay-- > 0);
+   free(addr1);
+   free(addr2);
+   free(addr3);
+   while(1);
 }
